08123b98b02a1c1dc33cf48384d7ca66060e4551
yc3841 Chen <yc3841@columbia.edu>
2023-10-05 06:15:05 +0000

    v1.11

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ActiveRecord::Base
+  #Movie.all_ratings to return all possible values.
+  def self.all_ratings
+    return self.find(:all, :select => "rating", :group => "rating").map(&:rating)
+  end
+
   def self.with_ratings(ratings_list)
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
